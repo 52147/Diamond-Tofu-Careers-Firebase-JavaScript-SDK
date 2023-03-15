40671f9007321f26901db878ed006d41e344ed84
Shou-Tzu Han <debra4117@gmail.com>
2023-03-14 23:38:35 -0400

    update search function

--- src/components/table/TableComponent.jsx
+++ src/components/table/TableComponent.jsx
@@ -1,88 +1,76 @@
 import React, { useState, useEffect } from "react";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, off, query, orderByChild, equalTo } from "firebase/database";
 
 export const TableComponent = () => {
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
-  const [filteredData, setFilteredData] = useState(data);
 
   useEffect(() => {
     const db = getDatabase();
     const dbRef = ref(db, "testuser");
+    let q = query(dbRef);
 
-    onValue(
-      dbRef,
-      (snapshot) => {
-        const items = [];
-        snapshot.forEach((childSnapshot) => {
-          const childData = childSnapshot.val();
-          console.log(childData); // Check if the expected data is being fetched
-          items.push(childData);
-        });
-        console.log(items); // Check if the expected data is being stored in the array
-        console.log(items.email);
-        setData(items);
-      },
-      {
-        onlyOnce: true,
-      }
+    if (searchQuery) {
+      q = query(
+        dbRef,
+        orderByChild("last_name"),
+        equalTo(searchQuery.toLowerCase().trim())
+      );
+    }
+
+    const handleData = (snapshot) => {
+      const items = [];
+      snapshot.forEach((childSnapshot) => {
+        const childData = childSnapshot.val();
+        console.log(childData); // Check if the expected data is being fetched
+        items.push({ ...childData, id: childSnapshot.key });
+      });
+      console.log(items); // Check if the expected data is being stored in the array
+      setData(items);
+      setFilteredData(items);
+    };
+
+    const handleError = (error) => {
+      console.error(error);
+    };
+
+    onValue(q, handleData, handleError);
+
+    return () => {
+      // Cleanup function to unsubscribe from Firebase queries
+      off(q, handleData);
+    };
+  }, [searchQuery]);
+
+  const handleSearch = (event) => {
+    const query = event.target.value.trim().toLowerCase();
+    setSearchQuery(query);
+    const filtered = data.filter((item) =>
+      item.last_name.toLowerCase().includes(query)
     );
-  }, []);
-
-  const handleSearch = async (event) => {
-    const lname = event.target.value;
-    const db = getDatabase();
-
-    onValue(
-      ref(db, `/testuser/${lname}`),
-      (snapshot) => {
-        console.log(lname);
-        const username = (snapshot.val() && snapshot.val()) || "Anonymous";
-        // ...
-        setFilteredData(username);
-      },
-      {
-        onlyOnce: true,
-      }
-    );
+    setFilteredData(filtered);
   };
 
   return (
     <>
       <div className="jobdes">
         <div className="container">
-          <div className="pt-3.5	 ">
-            <div className="text-center text-violet-700">
-              <h1>Form Data Table</h1>
+          <div className="pt-3.5 text-center text-violet-700">
+            <h1>Form Data Table</h1>
+          </div>
+
+          <br />
+          <div className="mb-3">
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search by Last Name"
+                onChange={handleSearch}
+              />
             </div>
 
-            <br />
-            <div className="mb-3">
-              <div className="input-group mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Search by Last Name"
-                  onChange={handleSearch}
-                />
-              </div>
-              <div>
-                <h1>First Name: {filteredData.first_name}</h1>
-              </div>
-              <div>
-                <h1>Last Name: {filteredData.last_name}</h1>
-              </div>
-              <div>
-                <h1>Location: {filteredData.location}</h1>
-              </div>
-              <div>
-                <h1>Email: {filteredData.email}</h1>
-                <h1>Education: {filteredData.education}</h1>
-                <h1>Accomplishment: {filteredData.accomplish}</h1>
-                <h1>Visa Status: {filteredData.visa}</h1>
-                <h1>Resume Link: {filteredData.resume}</h1>
-              </div>
-            </div>
             <div className="table-responsive">
               <table className="table table-bordered table-striped">
                 <thead>
@@ -98,7 +86,7 @@ export const TableComponent = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {data.map((item) => (
+                  {filteredData.map((item) => (
                     <tr key={item.id}>
                       <td>{item.first_name}</td>
                       <td>{item.last_name}</td>
